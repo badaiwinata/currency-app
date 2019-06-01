90b524e0fb97d29e515199f486be506d583e3c7f
badai <winata.afa@gmail.com>
2019-06-02 02:28:50 +0700

    adding add more function

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -4,14 +4,16 @@ import {
   MDBCard, MDBCardBody, MDBInput,
   MDBBtn, MDBIcon
 } from 'mdbreact';
-import AddMoreCurrencies from './AddMoreCurrencies';
 import axios from "axios";
+import Contoh from './contoh'
 
 export default class Converter extends Component {
 
   state = {
+    result: null,
     amount: 10,
-
+    toCurrency: 'GBP',
+    currencies: []
   }
 
   inputAmount = (e) => {
@@ -27,12 +29,21 @@ export default class Converter extends Component {
   componentDidMount() {
     axios.get("https://api.exchangeratesapi.io/latest?base=USD")
       .then(response => {
-        const currencyAr = ["USD"]
+        const currencyAr = ['USD']
         for (const key in response.data.rates) {
           currencyAr.push(key)
         }
         this.setState({ currencies: currencyAr.sort() })
       })
+      .catch(err => {
+        console.log("Opps", err.message);
+      });
+  }
+
+  selectCurrency = (e) => {
+    this.setState({
+      toCurrency: e.target.value
+    })
   }
 
   render() {
@@ -49,7 +60,7 @@ export default class Converter extends Component {
                 </MDBRow>
               </div>
 
-              <MDBCardBody className="mx-4 mt-4">
+              <MDBCardBody className="mx-4 mt-">
                 <MDBInput
                   label="USD - United States Dollars"
                   group
@@ -58,6 +69,7 @@ export default class Converter extends Component {
                   onChange={this.inputAmount}
                   validate />
                 <MDBBtn
+                  size="sm"
                   type="button"
                   color="orange"
                   rounded
@@ -65,11 +77,34 @@ export default class Converter extends Component {
                 >
                   <MDBIcon icon="exchange-alt" /> Convert
                 </MDBBtn>
-                <AddMoreCurrencies />
+                {this.state.result &&
+                  <h3>{this.state.result}</h3>
+                }
+
+                <div>
+                  <select
+                    className="browser-default custom-select"
+                    value={this.state.toCurrency}
+                    onChange={this.selectCurrency}
+                  >
+                    {this.state.currencies.map(cur => (
+                      <option key={cur}>{cur}</option>
+                    ))}
+                  </select>
+                  <MDBBtn
+                    size="sm"
+                    type="button"
+                    color="orange"
+                    rounded
+                  >
+                    <MDBIcon icon="exchange-alt" /> Add
+                </MDBBtn>
+                </div>
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
         </MDBRow>
+        <Contoh/>
       </MDBContainer>
     )
   }
